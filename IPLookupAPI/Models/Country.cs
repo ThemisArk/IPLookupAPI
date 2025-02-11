7d38195660cc16779b22da8818d1d1e198840154
Themis <themis.arkoudelis@gmail.com>
2025-02-11 17:08:14 +0200

    Created Country model

---
+++ IPLookupAPI/Models/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace IPLookupAPI.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string? CountryName { get; set; }
+        public string? TwoLetterCode { get; set; }
+        public string? ThreeLetterCode { get; set; }
+        public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
