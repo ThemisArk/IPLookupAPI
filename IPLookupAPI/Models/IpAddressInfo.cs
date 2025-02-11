e64f92eaaeb0b2ecf06c0bbc1b0bceed12b0f927
Themis <themis.arkoudelis@gmail.com>
2025-02-11 17:21:08 +0200

    Changed Country model to include only 1 model

--- IPLookupAPI/Models/IpAddressInfo.cs
+++ IPLookupAPI/Models/IpAddressInfo.cs
@@ -1,12 +1,14 @@
 ﻿namespace IPLookupAPI.Models
 {
-    public class Country
+    public class IpAddressInfo
     {
         public int Id { get; set; }
+        public int Ip {  get; set; }
         public string? CountryName { get; set; }
         public string? TwoLetterCode { get; set; }
         public string? ThreeLetterCode { get; set; }
-        public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
-
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        
     }
 }
