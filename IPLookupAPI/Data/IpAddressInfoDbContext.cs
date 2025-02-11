fd6ec7e4c2c1b3d729a209e391b5af122a80d88b
Themis <themis.arkoudelis@gmail.com>
2025-02-11 17:43:27 +0200

    Created IpAddress Database + added to programm + added connection string

---
+++ IPLookupAPI/Data/IpAddressInfoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using IPLookupAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IPLookupAPI.Data
+{
+    public class IpAddressInfoDbContext : DbContext
+    {
+        public IpAddressInfoDbContext(DbContextOptions<IpAddressInfoDbContext> options)
+            : base(options) { }
+
+        public DbSet<IpAddressInfo> IpAddressInfos { get; set; }
+    }
+}
--- IPLookupAPI/Program.cs
+++ IPLookupAPI/Program.cs
@@ -1,3 +1,6 @@
+using IPLookupAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace IPLookupAPI
 {
     public class Program
@@ -5,6 +8,11 @@ namespace IPLookupAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            //Add db
+            builder.Services.AddDbContext<IpAddressInfoDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
