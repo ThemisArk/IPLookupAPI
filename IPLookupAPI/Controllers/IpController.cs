307f0abf023044ccbc55b0e7743772885d0e8f83
Themis <themis.arkoudelis@gmail.com>
2025-02-11 18:37:52 +0200

    Created IpController

---
+++ IPLookupAPI/Controllers/IpController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IPLookupAPI.Controllers
+{
+    [Route("api/ip")]
+    [ApiController]
+    public class IpController : ControllerBase
+    {
+        
+    }
+}
