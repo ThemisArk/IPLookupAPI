94af467f6e8e8e0b0779d0c841a3b10fe85882b1
Themis <themis.arkoudelis@gmail.com>
2025-02-11 17:07:36 +0200

    Add project files.

---
+++ IPLookupAPI/Program.cs
@@ -0,0 +1,15 @@
+namespace IPLookupAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
